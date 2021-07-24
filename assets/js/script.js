6fc8706658c65b4c8db94dd27fedec7079403f4c
Teoman <teoman.h.aydogan@gmail.com>
2021-07-24 10:11:24 +1000

    now renders card after search

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,17 +4,32 @@ function get(x) {
     return document.getElementById(x)
 }
 
-function renderCards(condition, temp, country) {
-    
+function renderCards(condition, temp, city, country) {
+    weather_sec = get('weather_sec');
+
+    if (condition == 'Clear') {
+        console.log('clear icon')
+    }
+
+    else if (condition == 'Clouds') {
+        console.log('clouds icon')
+    }
+
+    weather_sec.innerHTML = "<div class='card weather_card'> <p class='city'>"+city+"</p> <p>"+country+"</p> <p class='temp'>"+temp+"°C</p> <p class='condition'>"+condition+"</p></div>"
 }
 
 function weatherData(data) {
     var condition = data['weather'][0]['main'];
-    var temp = data['main']['temp'];
+    var temp = Math.trunc(data['main']['temp']);
     var country = data['sys']['country']
-    console.log(condition, temp, country);
+    var city = data['name']
+    var icon = data['weather'][0]['icon']
 
-    return renderCards(condition, temp, country)
+    localStorage.setItem('pastSearches', city)
+
+    console.log(condition, temp, city, country, icon);
+
+    return renderCards(condition, temp, city, country)
 }
 
 function openApi() {
